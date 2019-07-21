9297d286ae8db7a2e6b87b5be1eab0b4ff4166ce
amarg <amaranatha.gowni@sap.com>
2019-07-22 00:13:44 +0800

    Anagram verified

--- SumPair.java
+++ SumPair.java
@@ -6,7 +6,19 @@ import java.util.Set;
 
 public class SumPair {
 	public static void main(String args[]) { 
+		
+/*		Random Integer array : [11, 8, 5, 6, 3, 5, 4, 13, 1]
+				Sum : 11
+				pair of numbers from an array whose sum equals 11
+				(6, 5) 
+				(3, 8) */
 		prettyPrint(getRandomArray(9), 11); 
+/*		Random Integer array : [4, 0, 8, 2, 8, 6, 5, 13, 3, 7]
+				Sum : 12
+				pair of numbers from an array whose sum equals 12
+				(8, 4) 
+				(8, 4) 
+				(7, 5) */
 		prettyPrint(getRandomArray(10), 12); 
 		}
 
