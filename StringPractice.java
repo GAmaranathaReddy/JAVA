16757ea44e1b7a7d77eda381dbf1169dd31b02e9
amarg <amaranatha.gowni@sap.com>
2019-07-21 23:05:24 +0800

    first Commit

---
+++ StringPractice.java
@@ -0,0 +1,23 @@
+package com.sample.string;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StringPractice {
+public static void main(String[] args) {
+	String s1="anagramm";
+	String s2="marganaa";
+	System.out.println(checkAnagrams(s1, s2));
+}
+
+public static boolean checkAnagrams(String s1,String s2) {
+	Map<Object, Long> countString=Arrays.stream(s1.toLowerCase().split(""))
+			.collect(Collectors.groupingBy(c ->c,Collectors.counting()));
+	Map<Object,Long> countString2=Arrays.stream(s2.toLowerCase().split("")).collect(Collectors.groupingBy(c -> c,Collectors.counting()));
+	Optional<Entry<Object, Long>> filterCounting=countString.entrySet().stream().filter(e -> countString.get(e)==countString2.get(e)).findFirst();
+	return !filterCounting.isPresent();
+}
+}
